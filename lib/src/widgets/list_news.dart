22549cbbdd0109f3a285e7571cd3d2f1d5d30dd5
Jorge Luis Alonso <alonso.jorgeluis.01@gmail.com>
2021-10-07 18:28:50 -0500

    News design part 2

--- lib/src/widgets/list_news.dart
+++ lib/src/widgets/list_news.dart
@@ -29,21 +29,74 @@ class _News extends StatelessWidget {
       children: [
         _TargetTopBar(news, index),
         _TargetTitle(news),
-        _TargetImage(url: news.url),
+        _TargetImage(url: news.urlToImage),
+        _TargetBody(news),
+        _TargetBottons(),
+        const SizedBox(height: 10.0),
+        const Divider()
+
+
 
       ],
     );
   }
 }
 
-class _TargetImage extends StatelessWidget {
-  final String url;
+class _TargetBottons extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        RawMaterialButton(
+          onPressed:(){},
+          fillColor: Colors.red,
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+          child: const Icon(Icons.star_border),
+        ),
+        const SizedBox(width: 10.0,),
+        RawMaterialButton(
+          onPressed:(){},
+          fillColor: Colors.blue,
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+          child: const Icon(Icons.animation_sharp),
+        )
+      ],
+    );
+  }
+}
 
+class _TargetBody extends StatelessWidget {
+  final Article news;
+  const _TargetBody(this.news);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+      child: Text((news.description != null) ? news.description! : ''),
+    );
+  }
+}
+
+class _TargetImage extends StatelessWidget {
+  final String? url;
   const _TargetImage({required this.url});
 
   @override
   Widget build(BuildContext context) {
-    return Text(url);
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 10.0),
+      child: ClipRRect(
+        borderRadius: const BorderRadius.only(topLeft: Radius.circular(50.0), bottomRight: Radius.circular(50.0)),
+        child: Container(
+          child: (url != null) ? FadeInImage(
+            placeholder: const AssetImage('assets/giphy.gif'),
+            image: NetworkImage(url!),
+          ): const Image(image: AssetImage('assets/no-image.png')),
+        ),
+      ),
+    );
   }
 }
 
